647f83100cb46000dd4b3f0a298feccd68470710
Flock92 <113182075+Flock92@users.noreply.github.com>
2023-09-07 23:41:47 +0100

    Update and rename constant.py to Constant.py

---
+++ apit212/Constant.py
@@ -0,0 +1,18 @@
+HEADERS = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+    "Cookie": ""
+}
+
+
+
+URL = 'https://www.trading212.com/'
+LOGIN_BUTTON = '//*[@id="__next"]/header/div/div/div[2]/div[1]/div[2]/p'
+COOKIE_POPUP = '/html/body/div[1]/main/div[3]/div/div[2]/div[2]/div[2]/div[1]'
+SUBMIT_BUTTON = '/html/body/div[1]/main/div[3]/div/div[2]/div/div[2]/div/form/div[5]'
+POPUP_CLOSE = '//*[@id="app"]/div[6]/div/div[2]/div/div/div/div[2]/div/div[1]/div[3]/div/span/div'
+REMEBER_ME_TOGGLE = '//*[@id="__next"]/main/div[2]/div/div[2]/div/div[2]/div/form/div[4]/div[2]/input'
+USER_NAME = '//*[@id="app"]/div[5]/div[1]/div/div[2]/div[3]/div/div[1]/div[1]'
+START_I_MENU = ['/html/body/div[1]/div[5]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div[3]/div','/html/body/div[1]/div[6]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div[3]/div']
