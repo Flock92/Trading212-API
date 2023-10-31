aefdc897f16d6fb1be396f3ae05c2b79951e1ddf
Flock92 <113182075+Flock92@users.noreply.github.com>
2023-10-31 21:34:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = """This is an unofficial API use to interact with the trading
 
 setup(
     name="apit212",
-    version="2.0.6",
+    version="3.0.1",
     packages=find_packages(),
     requires=[
         'requests',
